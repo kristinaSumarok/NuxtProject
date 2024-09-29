c7b85552331457f01cdea50f156518cd5bd7f2b4
kristina Troitski <ktroitski33@gmail.com>
2024-09-29 11:21:05 +0300

    configuring pages routing

--- app.vue
+++ app.vue
@@ -1,14 +1,27 @@
 <template>
   <body>
     <NuxtLayout>
+      <header>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/planner">Planner</NuxtLink></li>
+        </ul>
+      </header>
       <NuxtPage />
     </NuxtLayout>
 </body>
 </template>
 
+
 <style scoped>
 body {
   background-color: black;
+}
+div {
+  color: #84cc16;;
+}
+li {
+  color:aquamarine;
 }
 </style>
 
---
+++ pages/planner.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <h1>Planner page</h1>
+      <p>Lets create your TODO list</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
